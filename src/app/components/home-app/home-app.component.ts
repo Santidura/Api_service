a051a02c3cada2c1992e015d54e4fd1fff743be1
santiago durango <durangosantiago78@gmail.com>
2023-06-07 13:07:58 -0500

    angular

--- src/app/components/home-app/home-app.component.ts
+++ src/app/components/home-app/home-app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { compileNgModule } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from 'src/app/service/apiservice.service';
 
 @Component({
   selector: 'app-home-app',
   templateUrl: './home-app.component.html',
   styleUrls: ['./home-app.component.css']
 })
-export class HomeAppComponent {
+export class HomeAppComponent implements OnInit {
+
+  data: any[] =[]
+
+  constructor(private apijsonplaceholder : ApiserviceService){
+
+  }
+  ngOnInit(): void {
+    this.filldata();
+  }
+
+  filldata(){
+    this.apijsonplaceholder.getapi().subscribe(data =>{
+      this.data = data;
+      console.log(this.data);
+    })
+  }
 
 }
